d91a0f1de31fe02b8a85a7bbaed001f56861ebe0
saraabotalebi <saraabotalebi.9877@gmail.com>
2022-11-14 00:18:53 +0330

    create clock component

--- challenges/2/YOUR-CODE-GOES-HERE/saraabotalebi/src/app-timer/app-timer.js
+++ challenges/2/YOUR-CODE-GOES-HERE/saraabotalebi/src/app-timer/app-timer.js
@@ -1,9 +1,10 @@
+import Clock from '../components/clock/clock';
 import './app-timer.scss';
 
 const AppTimer = ()=>{
   return(
     <div className="AppTimer">
-      
+      <Clock/>
     </div>
   )
 }
