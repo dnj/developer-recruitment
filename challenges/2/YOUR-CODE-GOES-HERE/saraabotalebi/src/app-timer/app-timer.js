8a980f48a1930b8320628cb663d3322c20360c69
saraabotalebi <saraabotalebi.9877@gmail.com>
2022-11-14 00:25:00 +0330

    create timer component

--- challenges/2/YOUR-CODE-GOES-HERE/saraabotalebi/src/app-timer/app-timer.js
+++ challenges/2/YOUR-CODE-GOES-HERE/saraabotalebi/src/app-timer/app-timer.js
@@ -1,10 +1,12 @@
-import Clock from '../components/clock/clock';
+import Clock from './../components/clock/clock';
+import Timer from './../components/timer/timer';
 import './app-timer.scss';
 
 const AppTimer = ()=>{
   return(
     <div className="AppTimer">
       <Clock/>
+      <Timer activeHours={[{start:5,end:13},{start:22,end:24}]}/>
     </div>
   )
 }
--- challenges/2/YOUR-CODE-GOES-HERE/saraabotalebi/src/components/timer/timer.jsx
+++ challenges/2/YOUR-CODE-GOES-HERE/saraabotalebi/src/components/timer/timer.jsx
@@ -1,9 +1,22 @@
 import './timer.scss';
-const Timer = ()=>{
+const Timer = ({activeHours})=>{
+    const getStyleTimer = (start,end)=>{
+        
+        const startTimer = start*15;
+        const endTimer = (end-start)*15-0.5;
+        return({
+            transform : `translate(-50%,-50%) rotate(${startTimer}deg)`,
+            backgroundImage: `conic-gradient(orangered 0deg ${endTimer}deg, transparent ${endTimer}deg)`,
+        });
+    }
     return(
-        <div>
+        <>
+            {activeHours.map((activeHour,index)=>(
+                <div key={index} className="Timer" style={getStyleTimer(activeHour.start,activeHour.end)}></div>
+                ))
+            }
+        </>
 
-        </div>
     )
 }
 export default Timer;
