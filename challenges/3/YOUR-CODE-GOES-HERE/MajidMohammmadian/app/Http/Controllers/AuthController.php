dab21fcf28cebe1572753bb4796b9d6cb80da429
majeedmohammadian <majeedmohammadian@gmail.com>
2022-12-03 02:49:51 +0330

    complete user function

--- challenges/3/YOUR-CODE-GOES-HERE/MajidMohammmadian/app/Http/Controllers/AuthController.php
+++ challenges/3/YOUR-CODE-GOES-HERE/MajidMohammmadian/app/Http/Controllers/AuthController.php
@@ -1,16 +1,22 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Request;
 
-class AuthController extends Controller {
-	public function register(RegisterRequest $request) {
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
         $user = User::create($request);
 
         response()->json($user);
-	}
+    }
 
-	public function user() {
-	}
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
 }
