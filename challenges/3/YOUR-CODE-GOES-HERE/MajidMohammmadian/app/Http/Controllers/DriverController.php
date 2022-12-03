674560237e5e90b3d55ee10a5e2d25aa020e4cbd
majeedmohammadian <majeedmohammadian@gmail.com>
2022-12-03 14:38:09 +0330

    complete signup driver

--- challenges/3/YOUR-CODE-GOES-HERE/MajidMohammmadian/app/Http/Controllers/DriverController.php
+++ challenges/3/YOUR-CODE-GOES-HERE/MajidMohammmadian/app/Http/Controllers/DriverController.php
@@ -2,13 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\DriverStatus;
+use App\Http\Resources\DriverResource;
+use App\Models\Driver;
+use App\Http\Requests\DriverSignupRequest;
+
 class DriverController extends Controller
 {
-	public function signup()
-	{
-	}
+    public function signup(DriverSignupRequest $request)
+    {
+        $driver_exist = Driver::query()->where([
+            'car_plate' => $request->car_plate,
+            'car_model' => $request->car_model,
+        ])->exists();
 
-	public function update()
-	{
-	}
+        if($driver_exist) {
+            return response()->json([
+                'code' => 'AlreadyDriver'
+            ], 400);
+        }
+
+        $driver = new Driver;
+
+        $driver->id = auth()->id();
+        $driver->car_plate = $request->car_plate;
+        $driver->car_model = $request->car_model;
+        $driver->status = DriverStatus::NOT_WORKING->value;
+
+        $driver->save();
+
+        return response()->json(DriverResource::make($driver));
+    }
+
+    public function update()
+    {
+    }
 }
