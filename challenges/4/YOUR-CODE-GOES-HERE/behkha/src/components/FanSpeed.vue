2603653543653593092866fa8d119e13b9b5aa99
Behzad Khanlar <behkha@yahoo.com>
2022-12-21 16:40:53 +0330

    add fan speed

---
+++ challenges/4/YOUR-CODE-GOES-HERE/behkha/src/components/FanSpeed.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mx-3">
+    <div class="d-flex flex-row justify-content-end">
+      <h4>Speed</h4>
+    </div>
+    <input dir="rtl" type="range" class="w-100 mt-3"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'FanSpeed'
+})
+</script>
+
+<style scoped>
+input[type="range"] {
+  accent-color: var(--primary-color);
+}
+</style>
