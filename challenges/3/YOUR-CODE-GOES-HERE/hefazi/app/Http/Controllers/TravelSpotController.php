5496a1b13648b00595219892c0b22228ef540c53
Mohammad Hefazi <smohe1991@gmail.com>
2022-12-22 09:24:09 +0330

    init

---
+++ challenges/3/YOUR-CODE-GOES-HERE/hefazi/app/Http/Controllers/TravelSpotController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TravelSpotController extends Controller
+{
+	public function arrived()
+	{
+	}
+
+	public function store()
+	{
+	}
+
+	public function destroy()
+	{
+	}
+}
---
+++ challenges/3/YOUR-CODE-GOES-HERE/hefazi/app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+}
