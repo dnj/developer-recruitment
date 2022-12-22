30b3e0c7f8f14bf3da9f6e21cf322de5ebcd2a53
Mohammad Hefazi <smohe1991@gmail.com>
2022-12-22 23:57:32 +0330

    Some of users sign up for being a driver

--- challenges/3/YOUR-CODE-GOES-HERE/hefazi/app/Http/Controllers/DriverController.php
+++ challenges/3/YOUR-CODE-GOES-HERE/hefazi/app/Http/Controllers/DriverController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\DriverStatus;
+use App\Http\Requests\DriverSignupRequest;
+use App\Http\Resources\DriverResource;
+use App\Models\Driver;
+
 class DriverController extends Controller
 {
-	public function signup()
+	public function signup(DriverSignupRequest $request)
 	{
+		$exist = Driver::where([
+			'car_plate' => $request->car_plate,
+			'car_model' => $request->car_model,
+		])->exists();
+
+		if ($exist) {
+			return response()->json([
+				'code' => 'AlreadyDriver'
+			], 400);
+		}
+		$driver = Driver::create($request->all() + [
+			'id' => auth()->id(),
+			'status' => DriverStatus::NOT_WORKING->value
+		]);
+
+		return response()->json(DriverResource::make($driver));
 	}
 
 	public function update()
