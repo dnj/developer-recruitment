ae8f722b91ad61ab2722da4c465afae42eea1022
Behzad Khanlar <behkha@yahoo.com>
2022-12-22 11:21:23 +0330

    bind speed input range value to vuex store speed

--- challenges/4/YOUR-CODE-GOES-HERE/behkha/src/components/FanSpeed.vue
+++ challenges/4/YOUR-CODE-GOES-HERE/behkha/src/components/FanSpeed.vue
@@ -3,14 +3,22 @@
     <div class="d-flex flex-row justify-content-end">
       <h4>Speed</h4>
     </div>
-    <input dir="rtl" type="range" class="w-100 mt-3"/>
+    <input dir="rtl" type="range" value="1" min="1" max="3" step="1" class="w-100 mt-3" @change="onSpeedChange"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { mapMutations } from 'vuex';
+
 export default defineComponent({
-  name: 'FanSpeed'
+  name: 'FanSpeed',
+  methods: {
+    ...mapMutations(['updateSpeed']),
+    onSpeedChange(event: any) {
+      this.updateSpeed(event.target.value);
+    }
+  }
 })
 </script>
 
