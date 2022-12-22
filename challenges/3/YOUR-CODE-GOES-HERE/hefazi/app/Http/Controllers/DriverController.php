481ed778c54b64331074249ce8485f3e3b1399fb
Mohammad Hefazi <smohe1991@gmail.com>
2022-12-23 00:17:31 +0330

    Drivers send updates and retreive availble travels

--- challenges/3/YOUR-CODE-GOES-HERE/hefazi/app/Http/Controllers/DriverController.php
+++ challenges/3/YOUR-CODE-GOES-HERE/hefazi/app/Http/Controllers/DriverController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Enums\DriverStatus;
+use App\Enums\TravelStatus;
 use App\Http\Requests\DriverSignupRequest;
 use App\Http\Resources\DriverResource;
 use App\Models\Driver;
+use App\Models\Travel;
+use Illuminate\Http\Request;
 
 class DriverController extends Controller
 {
@@ -29,7 +32,20 @@ class DriverController extends Controller
 		return response()->json(DriverResource::make($driver));
 	}
 
-	public function update()
+	public function update(Request $request)
 	{
+		$driver = Driver::byUser(auth()->user());
+		$driver->update([
+			'latitude' => $request->latitude,
+			'longitude' => $request->longitude,
+			'status' => $request->status
+		]);
+
+		$travels = Travel::with('spots')->where('status', TravelStatus::SEARCHING_FOR_DRIVER->value)->get();
+
+		return response()->json([
+			'driver'  => $request->all(),
+			'travels' => $travels
+		]);
 	}
 }
