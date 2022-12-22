c14116a17652998c0755c0f1c66cf2ab0652dd57
Mohammad Hefazi <smohe1991@gmail.com>
2022-12-23 01:47:39 +0330

    Driver go to coordinates of first spot

--- challenges/3/YOUR-CODE-GOES-HERE/hefazi/app/Http/Controllers/TravelSpotController.php
+++ challenges/3/YOUR-CODE-GOES-HERE/hefazi/app/Http/Controllers/TravelSpotController.php
@@ -2,10 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\TravelStatus;
+use App\Exceptions\InvalidTravelStatusForThisActionException;
+use App\Exceptions\SpotAlreadyPassedException;
+use App\Models\Travel;
+use App\Models\TravelSpot;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+
 class TravelSpotController extends Controller
 {
-	public function arrived()
+	public function arrived(Travel $travel, TravelSpot $spot): JsonResponse
 	{
+		$travel = $travel->with('spots')->first();
+
+		if ($travel->passenger_id == auth()->id()) {
+			abort(403);
+		}
+
+		if ($travel->status == TravelStatus::CANCELLED) {
+			throw new InvalidTravelStatusForThisActionException();
+		}
+		
+		if ($travel->spots()->where('position', 0)->whereNotNull('arrived_at')->exists()) {
+			throw new SpotAlreadyPassedException();
+		}
+
+		$travel->spots()->where('position', 0)->update([
+			'arrived_at' => Carbon::now()
+		]);
+
+		return response()->json([
+			'travel' => $travel->with('spots')->first()->toArray()
+		]);
 	}
 
 	public function store()
