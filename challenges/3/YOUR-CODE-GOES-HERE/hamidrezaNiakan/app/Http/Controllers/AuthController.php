1848bf7b8a7f618214f71718e2d589d79e4a4294
niakan2088 <hamid.niakan1993@gmail.com>
2022-12-25 18:29:51 +0330

    create register user as passenger and show and also The test was done correctly

--- challenges/3/YOUR-CODE-GOES-HERE/hamidrezaNiakan/app/Http/Controllers/AuthController.php
+++ challenges/3/YOUR-CODE-GOES-HERE/hamidrezaNiakan/app/Http/Controllers/AuthController.php
@@ -1,10 +1,48 @@
 <?php
+
 namespace App\Http\Controllers;
 
-class AuthController extends Controller {
-	public function register() {
-	}
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-	public function user() {
+class AuthController extends Controller {
+	/**
+	 * Create new use as passenger
+	 *
+	 * @param \App\Http\Requests\RegisterRequest $request
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function register ( RegisterRequest $request ) {
+		$user = User::query()
+					->create([
+								 'cellphone' => $request->get('cellphone') ,
+								 'name' => $request->get('name') ,
+								 'lastname' => $request->get('lastname') ,
+								 'password' => Hash::make($request->get('password')) ,
+							 ]);
+		$user[ 'token' ] = $user->createToken('api-token')->plainTextToken;
+		
+		return response()->json([
+									'user' => UserResource::make($user) ,
+								]);
+	}
+	
+	/**
+	 * Display current user that is login
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function user ( Request $request ) {
+		$user = $request->user();
+		$user[ 'token' ] = $request->user()
+								   ->currentAccessToken()->token;
+		
+		return response()->json([
+									'user' => UserResource::make($user) ,
+								]);
 	}
 }
