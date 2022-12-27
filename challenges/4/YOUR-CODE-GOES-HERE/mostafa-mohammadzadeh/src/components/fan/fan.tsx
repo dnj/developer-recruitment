52fe9df74135603b8a4421db723cdaedc814b547
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-12-27 22:43:57 +0330

    components: fan created

---
+++ challenges/4/YOUR-CODE-GOES-HERE/mostafa-mohammadzadeh/src/components/fan/fan.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './fan.scss'
+
+type FanProps = {
+  isOn: boolean,
+  isOscillating: boolean,
+  speedStatus: number,
+  conditionStatus: string
+}
+
+export default function Fan({ isOn, isOscillating, speedStatus, conditionStatus }: FanProps){
+
+
+
+  return (
+    <div className='fan'>
+        
+      {/* fan */}
+      <div id='fanContainer'>
+
+        {/* body */}
+        <div id='fan-body'>
+          <div id='grid-layer-1'>
+            <div id='grid-layer-2'>
+              <div id='grid-layer-3'>
+                <div id='fan-cap'/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* waves */}
+      <div id='wave_3c859f'>
+        <span id='stand'/>
+      </div>
+      <div id='wave_7bdcb5'/>
+      <div id='wave_f6f6f6'/>
+    </div>
+  );
+}
