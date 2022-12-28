baeacf90e7e2a272d412a798d355198fd9ecae91
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-12-28 11:05:38 +0330

    speed controller: new mui theme

--- challenges/4/YOUR-CODE-GOES-HERE/mostafa-mohammadzadeh/src/components/speedController/speedController.tsx
+++ challenges/4/YOUR-CODE-GOES-HERE/mostafa-mohammadzadeh/src/components/speedController/speedController.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import Slider from '@mui/material/Slider'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import rtlPlugin from 'stylis-plugin-rtl'
+import { prefixer } from 'stylis'
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
 import './speedController.scss'
 
 type SpeedControllerProps = {
@@ -9,40 +14,62 @@ type SpeedControllerProps = {
 
 export default function SpeedController({ status, onChange }: SpeedControllerProps){
 
-
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   onChange(event.target.checked)
-  // };
-
+  //labels:
   const labels = [
     'کند',
     'آهسته',
     'متوسط',
     'سریع',
     'کولاک'
-  ]
+  ];
 
-
+  //handleChange:
   const handleChange = (event: Event, newValue: number | number[]) => {
     onChange(newValue as number);
   };
 
+  //create rtl cache:
+  const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+
+  //theme:
+  const theme = createTheme({
+    direction: 'rtl',
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      secondary: {
+        main: '#3c859f',
+        darker: '#3c859f',
+      },
+    },
+  });
+
+  //render:
   return (
-    <div className='speedController'>
-      <header id='header'>
-        <a id='title'>سرعت چرخش</a>
-      </header>
-      <div id='content'>
-        <Slider
-          value={status}
-          valueLabelDisplay="on"
-          valueLabelFormat={value => <div className="label">{labels[value]}</div>}
-          step={1}
-          min={0}
-          max={4}
-          onChange={handleChange}
-        />
-      </div>
-    </div>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <div className='speedController' dir="rtl">
+          <header id='header'>
+            <a id='title'>سرعت چرخش</a>
+          </header>
+          <div id='content'>
+            <Slider
+              value={status}
+              dir="rtl"
+              valueLabelDisplay="on"
+              valueLabelFormat={value => <div className="label">{labels[value]}</div>}
+              step={1}
+              min={0}
+              max={4}
+              color={"secondary"}
+              onChange={handleChange}/>
+          </div>
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
