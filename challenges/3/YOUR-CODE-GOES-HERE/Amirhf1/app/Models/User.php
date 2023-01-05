d1b104a0bc0cc791c8c07c24749115c864660029
amirhf <amirhacking85@gmail.com>
2023-01-05 18:17:02 +0330

    add getter/setter for user entity

--- challenges/3/YOUR-CODE-GOES-HERE/Amirhf1/app/Models/User.php
+++ challenges/3/YOUR-CODE-GOES-HERE/Amirhf1/app/Models/User.php
@@ -7,6 +7,21 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ *
+ * @package App\Models
+ *
+ * @property int       id
+ * @property string    name
+ * @property string    lastname
+ * @property string    cellphone
+ * @property string    password
+ * @property \DateTime created_at
+ * @property \DateTime updated_at
+ *
+ * @method static Builder filter($query, array $input = [], $filter = null)
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -27,5 +42,93 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCellphone(): string
+    {
+        return $this->cellphone;
+    }
+
+    /**
+     * @param string $cellphone
+     */
+    public function setCellphone(string $cellphone): void
+    {
+        $this->cellphone = $cellphone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updated_at;
+    }
 
 }
