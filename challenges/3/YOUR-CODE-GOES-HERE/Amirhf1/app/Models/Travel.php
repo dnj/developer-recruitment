0bb1214417d5199d0817c92365565a075e83f975
amirhf <amirhacking85@gmail.com>
2023-01-06 03:55:57 +0330

    add setter/getter for travel model

--- challenges/3/YOUR-CODE-GOES-HERE/Amirhf1/app/Models/Travel.php
+++ challenges/3/YOUR-CODE-GOES-HERE/Amirhf1/app/Models/Travel.php
@@ -7,11 +7,25 @@ use App\Enums\TravelStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class User
+ *
+ * @package App\Models
+ *
+ * @property int id
+ * @property int passenger_id
+ * @property int driver_id
+ * @property string status
+ * @property \DateTime created_at
+ * @property \DateTime updated_at
+ *
+ */
 class Travel extends Model
 {
     use HasFactory;
 
-    public static function userHasActiveTravel(User $user): bool {
+    public static function userHasActiveTravel(User $user): bool
+    {
         return self::query()
             ->whereIn("status", [TravelStatus::RUNNING, TravelStatus::SEARCHING_FOR_DRIVER])
             ->where(function ($q) use ($user) {
@@ -46,15 +60,18 @@ class Travel extends Model
         return $this->hasMany(TravelSpot::class);
     }
 
-    public function passengerIsInCar(): bool {
+    public function passengerIsInCar(): bool
+    {
         return $this->events()->where("type", TravelEventType::PASSENGER_ONBOARD)->exists();
     }
 
-    public function driverHasArrivedToOrigin(): bool {
+    public function driverHasArrivedToOrigin(): bool
+    {
         return $this->spots()->where("position", 0)->value("arrived_at") !== null;
     }
 
-    public function allSpotsPassed(): bool {
+    public function allSpotsPassed(): bool
+    {
         return $this->spots()->whereNull("arrived_at")->doesntExist();
     }
 
@@ -64,4 +81,77 @@ class Travel extends Model
             ->where("position", 0)
             ->firstOrFail();
     }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPassengerId(): int
+    {
+        return $this->passenger_id;
+    }
+
+    /**
+     * @param int $passenger_id
+     */
+    public function setPassengerId(int $passenger_id): void
+    {
+        $this->passenger_id = $passenger_id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDriverId(): int
+    {
+        return $this->driver_id;
+    }
+
+    /**
+     * @param int $driver_id
+     */
+    public function setDriverId(int $driver_id): void
+    {
+        $this->driver_id = $driver_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updated_at;
+    }
+
 }
