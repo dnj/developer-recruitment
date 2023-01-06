4a2a37d6e38abfd47118969fa6befe92dbc3d207
amirhf <amirhacking85@gmail.com>
2023-01-06 16:30:09 +0330

    add setter/getter in travel spot

--- challenges/3/YOUR-CODE-GOES-HERE/Amirhf1/app/Models/TravelSpot.php
+++ challenges/3/YOUR-CODE-GOES-HERE/Amirhf1/app/Models/TravelSpot.php
@@ -4,7 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * Class User
+ *
+ * @package App\Models
+ *
+ * @property int       id
+ * @property int       travel_id
+ * @property int       position
+ * @property double    latitude
+ * @property double    longitude
+ * @property string    status
+ * @property \DateTime created_at
+ * @property \DateTime updated_at
+ *
+ */
 class TravelSpot extends Model
 {
     use HasFactory;
@@ -13,5 +27,109 @@ class TravelSpot extends Model
 
     public function travel() {
         return $this->belongsTo(Travel::class);
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTravelId(): int
+    {
+        return $this->travel_id;
+    }
+
+    /**
+     * @param int $travel_id
+     */
+    public function setTravelId(int $travel_id): void
+    {
+        $this->travel_id = $travel_id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition(): int
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param int $position
+     */
+    public function setPosition(int $position): void
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLatitude(): float
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     */
+    public function setLatitude(float $latitude): void
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude(): float
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     */
+    public function setLongitude(float $longitude): void
+    {
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updated_at;
     }
 }
