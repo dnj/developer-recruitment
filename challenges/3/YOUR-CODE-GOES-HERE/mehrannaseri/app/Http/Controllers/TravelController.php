745cc950314faa99940dd171c55567b660deff86
mehran <mehran.naseri2013@gmail.com>
2023-01-06 10:37:47 +0330

    checked all conditions user (passenger or driver) can cancel travel or not

--- challenges/3/YOUR-CODE-GOES-HERE/mehrannaseri/app/Http/Controllers/TravelController.php
+++ challenges/3/YOUR-CODE-GOES-HERE/mehrannaseri/app/Http/Controllers/TravelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Enums\TravelStatus;
 use App\Http\Requests\TravelStoreRequest;
 use App\Http\Resources\TravelResource;
+use App\Models\Driver;
 use App\Models\Travel;
 
 class TravelController extends Controller
@@ -31,17 +32,21 @@ class TravelController extends Controller
 
 	public function cancel($travel_id)
 	{
-        $passenger = auth()->user();
+        $travel = Travel::findOrFail($travel_id);
 
-        $travel = Travel::searchingForDriver()
-            ->where('passenger_id', $passenger->id)
-            ->findOrFail($travel_id);
+        list($can_not_cancel, $message) = $this->CanNotCancelTravel($travel);
+        if($can_not_cancel){
+            return response()->json(['code' => $message], 400);
+        }
+        else{
+            $travel->update([
+                'status' => TravelStatus::CANCELLED->value
+            ]);
 
-        $travel->update([
-            'status' => TravelStatus::CANCELLED->value
-        ]);
+            return response()->json(TravelResource::make($travel));
+        }
 
-        return response()->json(TravelResource::make($travel));
+
 
 	}
 
@@ -56,4 +61,24 @@ class TravelController extends Controller
 	public function take()
 	{
 	}
+
+    private function CanNotCancelTravel($travel)
+    {
+        $user = auth()->user();
+        $can_not_cancel = false;
+        $message = '';
+
+        if(in_array($travel->status->value, [TravelStatus::CANCELLED->value, TravelStatus::DONE->value])){
+            $message = 'CannotCancelFinishedTravel';
+            $can_not_cancel = true;
+        }
+
+        if($travel->status->value == TravelStatus::RUNNING->value
+            and $travel->driverHasArrivedToOrigin() and ! Driver::isDriver($user) ){
+            $message = 'CannotCancelRunningTravel';
+            $can_not_cancel = true;
+        }
+
+        return [$can_not_cancel, $message];
+    }
 }
