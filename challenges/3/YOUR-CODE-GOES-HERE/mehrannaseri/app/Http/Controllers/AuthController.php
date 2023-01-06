0049ca759f4495f55f9e2a60bbd35125225be103
mehran <mehran.naseri2013@gmail.com>
2023-01-06 08:56:32 +0330

    implement register user and pass tests

--- challenges/3/YOUR-CODE-GOES-HERE/mehrannaseri/app/Http/Controllers/AuthController.php
+++ challenges/3/YOUR-CODE-GOES-HERE/mehrannaseri/app/Http/Controllers/AuthController.php
@@ -1,8 +1,24 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class AuthController extends Controller {
-	public function register() {
+	public function register(RegisterRequest $request) {
+        $data = $request->validated();
+        $data['password'] = Hash::make($request->password);
+
+        $user = User::create($data);
+
+        if($user)
+            $user->token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => UserResource::make($user)
+        ]);
 	}
 
 	public function user() {
